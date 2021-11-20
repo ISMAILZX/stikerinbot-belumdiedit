197d43750892aa2fd1908fdc7616418c99d878c5
IG@Ismailreal.id <77217686+ismailofficial@users.noreply.github.com>
2021-11-20 21:13:26 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -20,8 +20,8 @@ const defaultMenu = {
 │
 ├ Uptime: *%uptime (%muptime)*
 ├ Database: %rtotalreg dari %totalreg
-├ Github:
-├ %github
+├ YouTube:
+├  https://youtube.com/c/ismailkurama
 └────
 %readmore`.trimStart(),
   header: '┌─〔 %category 〕',
@@ -300,42 +300,42 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
         }
       }, {}), { waitForAck: true })
     }
-    // gunakan ini jika kamu menggunakan whatsapp bisnis
-    //   throw `
-    // ┌〔 DAFTAR MENU 〕
-    // ├ ${_p + command} all
-    // ├ ${_p + command} game
-    // ├ ${_p + command} xp
-    // ├ ${_p + command} stiker
-    // ├ ${_p + command} kerang
-    // ├ ${_p + command} quotes
-    // ├ ${_p + command} admin
-    // ├ ${_p + command} group
-    // ├ ${_p + command} premium
-    // ├ ${_p + command} internet
-    // ├ ${_p + command} anonymous
-    // ├ ${_p + command} nulis
-    // ├ ${_p + command} downloader
-    // ├ ${_p + command} tools
-    // ├ ${_p + command} fun
-    // ├ ${_p + command} database
-    // ├ ${_p + command} vote
-    // ├ ${_p + command} quran
-    // ├ ${_p + command} audio
-    // ├ ${_p + command} jadibot
-    // ├ ${_p + command} info
-    // ├ ${_p + command} tanpa kategori
-    // ├ ${_p + command} owner
-    // └────  
-    //     `.trim()
+     gunakan ini jika kamu menggunakan whatsapp bisnis
+       throw `
+     ┌〔 DAFTAR MENU 〕
+     ├ ${_p + command} all
+     ├ ${_p + command} game
+     ├ ${_p + command} xp
+     ├ ${_p + command} stiker
+     ├ ${_p + command} kerang
+     ├ ${_p + command} quotes
+     ├ ${_p + command} admin
+     ├ ${_p + command} group
+     ├ ${_p + command} premium
+     ├ ${_p + command} internet
+     ├ ${_p + command} anonymous
+     ├ ${_p + command} nulis
+     ├ ${_p + command} downloader
+     ├ ${_p + command} tools
+     ├ ${_p + command} fun
+     ├ ${_p + command} database
+     ├ ${_p + command} vote
+     ├ ${_p + command} quran
+     ├ ${_p + command} audio
+     ├ ${_p + command} jadibot
+     ├ ${_p + command} info
+     ├ ${_p + command} tanpa kategori
+     ├ ${_p + command} owner
+     └────  
+         `.trim()
     let groups = {}
     for (let tag in tags) {
       groups[tag] = []
       for (let plugin of help)
         if (plugin.tags && plugin.tags.includes(tag))
           if (plugin.help) groups[tag].push(plugin)
-      // for (let tag of plugin.tags)
-      //   if (!(tag in tags)) tags[tag] = tag
+       for (let tag of plugin.tags)
+         if (!(tag in tags)) tags[tag] = tag
     }
     conn.menu = conn.menu ? conn.menu : {}
     let before = conn.menu.before || defaultMenu.before
